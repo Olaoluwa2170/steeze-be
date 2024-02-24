43262c4c0f4441be2ec2bf3608e2ba11876f7114
Babalola Elisha <elitebabson@gmail.com>
2024-02-24 23:36:19 +0100

    chore: set up database services

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,7 +1,14 @@
 import { Injectable } from '@nestjs/common';
+import { Prisma } from '@prisma/client';
 import { DatabaseService } from 'src/database/database.service';
 
 @Injectable()
 export class AuthService {
   constructor(private readonly databaseService: DatabaseService) {}
+
+  async signUp(createUserDto: Prisma.UserCreateInput) {
+    return await this.databaseService.user.create({
+      data: createUserDto,
+    });
+  }
 }
